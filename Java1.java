55463324e0ef347ae5346223979bce0a37260bd8
Sandra <sandracollehkayeh@gmail.com>
2025-11-17 09:16:35 -0800

    adding files

--- Java1.java
+++ Java1.java
@@ -10,3 +10,59 @@
  * the class should be define in the main class of the program so the methods can be called from the main method.
  * 
  */
+import java.util.Scanner;
+ public class Java1{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        java1_operation oop=new java1_operation();
+        int L, W, reactangle;
+        double r, circle;
+        int b, h, triangle;
+        System.out.println("Area Of Different Shapes");
+        System.out.println("*******************************");
+        System.out.printf("1.reactangle\n2.circle\n3.triangle\n4.exit\n5");
+        System.out.println("*******************************");
+        int choice;
+        System.out.println("Enter the choice: ");
+        choice=input.nextInt();
+        switch (choice) {
+            case 1:
+            System.out.println("Enter the first number: ");
+            L=input.nextInt();
+            System.out.println("Enter the second number: ");
+            W=input.nextInt();
+            reactangle=oop.reactangle(W, L);
+            System.out.println(reactangle);
+
+            break;
+        case 2:
+         System.out.println("Enter the first number: ");
+            r=input.nextDouble();
+            circle= oop.circle(r);
+            System.out.println(circle);
+            break;
+        case 3:
+         System.out.println("Enter the first number: ");
+            b=input.nextInt();
+            System.out.println("Enter the second number: ");
+            h=input.nextInt();
+            triangle=oop.triangle(b, h);
+            System.out.println(triangle);
+        case 4:
+         System.out.println("Thank you for using this code.");
+         break;
+         default:
+         System.out.println("Enter the right number.");
+
+        }
+        input.close();
+
+
+    }
+    
+
+
+
+
+}
+
--- java1_operation.java
+++ java1_operation.java
@@ -1,3 +1,22 @@
 public class java1_operation {
+    int reactangle(int W, int L){
+        return W*L;
+    }
+    double circle(double  r ){
+        return 3.14*r*r;
+    }
+
+    int triangle(int b, int h){
+        return (b*h)/2;
+    }
+        
+
+        
+
+
+
+
+
+    }
     
-}
+
